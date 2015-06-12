12307c956f1ecd2cf8f63c816bc12b79509d6b9a
bgrove1974 <bgroveben@gmail.com>
2015-06-12 03:08:21 -0700

    Change grid to 64 by 64 pixels

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 	var $table = $("#grid");
 	var html = [];
 	var row, col;
-	for (row=0; row < 16; row++) {
+	for (row=0; row < 64; row++) {
 		html.push("<tr class='row'>");
-		for(col=0; col < 16; col++) {
+		for(col=0; col < 64; col++) {
 			html.push("<td class='col'></td>");
 		}
 		html.push("</tr>");
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
 	$("td").hover(function() {
 		$(this).addClass("hover");
-	}, function() {
-		$(this).removeClass("hover");
+	// }, function() {
+		// $(this).removeClass("hover");
 	});
 });
