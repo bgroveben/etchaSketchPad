8ada928276a2e473b40fd76bc9c253714aca1e6d
bgrove1974 <bgroveben@gmail.com>
2015-06-12 05:19:50 -0700

    Make a button

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
+	var grid_size = prompt("How many rows in your grid?");
+	// var cell_size = $("#grid").width()/input - 2
 	var $table = $("#grid");
 	var html = [];
 	var row, col;
-	for (row=0; row < 64; row++) {
+	for (row=0; row < grid_size; row++) {
 		html.push("<tr class='row'>");
-		for(col=0; col < 64; col++) {
+		for(col=0; col < grid_size; col++) {
 			html.push("<td class='col'></td>");
 		}
 		html.push("</tr>");
 	}
 	$table.append(html.join(''));
+
+	// $(".row").css("width", cell_size);
+	// $(".col").css("height", sell_size);
 
 	$("td").hover(function() {
 		$(this).addClass("hover");
